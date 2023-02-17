b6f1a222bf9ab88e989cc5dac554bac2913a5be6
“Nababa <nababa3@github.com>
2023-02-17 13:35:16 -0800

    Print Alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,31 +1,18 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
  * main - entry point
- * Return: always 0 (success)
+ * Return: 0 (success)
  */
 int main(void)
 {
-	int n;
-	int lastn;
+	char 1case = 'a'
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	lastn = n % 10;
-	printf("Last digit of ");
-	printf("%d", n);
-	printf(" is %d ', lastn);
-	if (lastn > 5)
+	while (1case <= 'z')
 	{
-		printf("and is greater than 5\n");
+		putchar (1case);
+		1case++;
 	}
-	{
-	else if (lastn == 0)
-	}
-	{
-		printf('and is less than 6 and not 0\n");
-	}
+	putchar ('\n');
 	return (0);
 }
