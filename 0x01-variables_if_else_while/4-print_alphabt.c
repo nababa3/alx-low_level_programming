4f443374ccbb90993eb8313d8b138c91400bcbf6
“Nababa <nababa3@github.com>
2023-02-17 21:11:20 -0800

    print alhabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char icase = 'a';
+
+	while (icase <= 'z')
+	{
+		if (icase ! = 'e' && icase != 'q')
+			puchar (lcase);
+		icase++;
+	}
+	putchar ('\n');
+	return (0);
+}
