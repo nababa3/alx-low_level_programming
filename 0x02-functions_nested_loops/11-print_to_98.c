72d701bb9ca81811f8a1d1e0b24950b18aa17c90
“Nababa <nababa3@github.com>
2023-02-21 14:42:23 -0800

    11 print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,3 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - Prints all natural numbers from n to 98
+ * @n: Starting number
+ *
+ * Return: Always 0
+ */
 void print_to_98(int n)
 {
 	if (n >= 98)
