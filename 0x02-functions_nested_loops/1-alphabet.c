574695cc8d38eb113c515b93ec92808f53a76baf
“Nababa <nababa3@github.com>
2023-02-21 06:44:08 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*
+ * print_alphabet - print alphabets a-z
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	int 1 = 'a';
+
+	while (1 <= 'z')
+	{
+		_putchar(1);
+		1++;
+	}
+	_putchar('\n');
+}
