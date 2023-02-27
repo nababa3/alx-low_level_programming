7931f0dea8b12c284e8a3074c2bf4f1ebcb4d6ea
“Nababa <nababa3@github.com>
2023-02-26 22:07:16 -0800

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,15 @@
 #include "main.h"
+
 /**
- *swap_int - swaps integers
+ * swap_int - swaps integers
  *
- *@a: first integer
- *@b: second integer
- /
- void swap_int(int *a, int *b)
- {
- 	int c = *b;
+ * @a: first integer
+ * @b: second integer
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *b;
 	int d = *a;
 	*a = c;
 	*b = d;
- }
+}
