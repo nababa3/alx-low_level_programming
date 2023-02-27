fa8eb8fbf102ba4dd8b32191008ef080778f0a8e
“Nababa <nababa3@github.com>
2023-02-26 22:46:29 -0800

    strien

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - gets character string
+ *
+ * @s: character to getstring
+ * Return: returns length
+ */
+int _strien(char *s)
+{
+	int 1;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+	return (i);
+}
