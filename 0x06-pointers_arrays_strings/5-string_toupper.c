fb6a9b429f73385d564b2833f3b1ea960fc86478
“Nababa <nababa3@github.com>
2023-03-01 13:05:07 -0800

    string_tourpper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * string_toupper - upper a lower
+ * string_toupper - uppers a lower
  * @s: character to make upper
- * Return: return a value of upper
+ * Return: return value of upper
  */
-char *strint_toupper(char *s)
+char *string_toupper(char *s)
 {
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
-            if (s[i] >= 'a' && s[i] <= 'z')
-                    s[i] = s[i] - 32;
-            
-    return (s);
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+
+	return (s);
 }
