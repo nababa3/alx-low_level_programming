ff9e3763710df6e0051b349035c6da0f5dd0b870
“Nababa <nababa3@github.com>
2023-03-01 12:42:59 -0800

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
  * _strcmp - string to compare
- * @s1: destination to copy
- * @s2: source of copy
- * 
+ * @s1: first string
+ * @s2: second string
+ *
  * Return: value of string
  */
-char *_strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
 	while (s1[i] - s2[i] == 0 && s1[i] != '\0')
-    {
-     	i++;   
+	{
+		i++;
 	}
 	return (s1[i] - s2[i]);
 }
