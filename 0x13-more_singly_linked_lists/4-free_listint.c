52bd367e9862da133fed6cb1035af034426ccd43
“Nababa <nababa3@github.com>
2023-04-03 16:26:09 -0700

    free_listint

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,19 +1,18 @@
+#include <stdlib.h>
 #include "lists.h"
+
 /**
- * free_listint - frees the list
- * @head: head of struct
- *
+ * free_listint - Free all nodes from a list
+ * @head: Pointer to the start of the list
  */
 void free_listint(listint_t *head)
 {
 	listint_t *tmp;
 
-
 	while (head != NULL)
 	{
 		tmp = head;
-		free(head);
-		head = tmp->next;
+		head = head->next;
+		free(tmp);
 	}
-
 }
