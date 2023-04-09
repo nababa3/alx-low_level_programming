569d26b8a65f13a5de6fdf3d6a7d3e7ea143f46b
“Nababa <nababa3@github.com>
2023-04-09 02:56:58 -0700

    binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,19 +7,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int x = 0;
+	unsigned int n = 0;
 
 	if (b == NULL)
 		return (0);
 	while (*b)
 	{
 		if (*b == '1')
-			x = (x << 1) | 1;
+			n = (n << 1) | 1;
 		else if (*b == '0')
-			x <<= 1;
+			n <<= 1;
 		else
 			return (0);
 		b++;
 	}
-	return (x);
+	return (n);
 }
