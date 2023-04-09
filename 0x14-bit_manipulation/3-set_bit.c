ded595d747857c88b11d186f3b22a46a460b0a58
“Nababa <nababa3@github.com>
2023-04-09 03:11:51 -0700

    3-set

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * set_bit - sets a bit
+ * @n: integer to grab 
+ *
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 32)
+		return (-1);
+	(*n) |= 1 << index;
+	if (get_bit((*n), index) == 1)
+		return (1);
+	return (-1);
+}
