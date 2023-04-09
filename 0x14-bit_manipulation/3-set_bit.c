cacb1f1c7facbc1034dda2e13931e3ec77702043
“Nababa <nababa3@github.com>
2023-04-09 03:14:20 -0700

    3-set

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,15 +1,18 @@
 #include "main.h"
+
 /**
- * set_bit - sets a bit
- * @n: integer to grab 
+ * set_bit - Sets the value of a bit.
+ * @n: pointer to the bit.
+ * @index: index to set the value at.
  *
+ * Return: 1 if it worked, or -1 if an error occurred.
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 32)
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	(*n) |= 1 << index;
-	if (get_bit((*n), index) == 1)
-		return (1);
-	return (-1);
+
+	*n ^= (1 << index);
+
+	return (1);
 }
