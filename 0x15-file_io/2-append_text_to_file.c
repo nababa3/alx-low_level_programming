1f684f7db356b5111b91f5f6b4fff6858098fd52
“Nababa <nababa3@github.com>
2023-04-09 10:00:12 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,7 +10,7 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int p, r, len = 0;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -21,13 +21,13 @@ int append_text_to_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	p = open(filename, O_WRONLY | O_APPEND);
-	r = write(o, text_content, len);
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
 
-	if (p == -1 || r == -1)
+	if (o == -1 || w == -1)
 		return (-1);
 
-	close(p);
+	close(o);
 
 	return (1);
 }
