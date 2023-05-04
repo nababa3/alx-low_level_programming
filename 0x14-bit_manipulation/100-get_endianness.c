d1c2a9b2585a320b6915725523d87d38bf20f75d
“Nababa <nababa3@github.com>
2023-05-04 16:07:21 -0700

    endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -7,11 +7,8 @@
  */
 int get_endianness(void)
 {
-	int n = 1;
-	char *endian = (char *)&n;
+	unsigned int n = 1;
+	char *endian = (char *) &n;
 
-	if (*endian == 1)
-		return (1);
-
-	return (0);
+	return (*endien);
 }
