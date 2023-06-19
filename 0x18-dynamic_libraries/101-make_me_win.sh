52f8a984aa5a1ec168044d8f9b249b166dc6f0ce
“Nababa <nababa3@github.com>
2023-06-19 13:50:05 -0700

    make

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/nababa3/alx-low_level_programming/raw/master/0x18-dynamic_libraies
-export LD_PRELOAD=rand.so
+wget -P /tmp https://github.com/nababa3/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
